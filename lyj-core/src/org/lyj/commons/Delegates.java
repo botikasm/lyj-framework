b40e5d23339f3449886bfd70a636dc5e97ef9aec
gian angelo geminiani <angelo.geminiani@gmail.com>
2016-05-19 15:35:39 +0200

    no message

--- lyj-core/src/org/lyj/commons/Delegates.java
+++ lyj-core/src/org/lyj/commons/Delegates.java
@@ -97,7 +97,7 @@ public class Delegates {
 
     @FunctionalInterface
     public static interface SingleResultCallback<T> {
-        void handle(final Throwable err, final T data) throws Exception;
+        void handle(final Throwable err, final T data);
     }
 
     @FunctionalInterface
