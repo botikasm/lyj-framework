0b2a25a8032ed5c942cb1c357cab0a62a9f7ad41
gian angelo geminiani <angelo.geminiani@gmail.com>
2017-10-12 18:38:23 +0200

    no message

--- lyj-core/src/org/lyj/commons/lang/ValueObject.java
+++ lyj-core/src/org/lyj/commons/lang/ValueObject.java
@@ -9,6 +9,7 @@ public class ValueObject<T> {
     //                      f i e l d s
     // ------------------------------------------------------------------------
 
+    private T _default_value;
     private T _value;
 
     // ------------------------------------------------------------------------
@@ -32,8 +33,16 @@ public class ValueObject<T> {
     //                      p u b l i c
     // ------------------------------------------------------------------------
 
+    public T defaultValue() {
+        return _default_value;
+    }
+
+    public void defaultValue(final T value) {
+        _default_value = value;
+    }
+
     public T content() {
-        return _value;
+        return _value != null ? _value : _default_value;
     }
 
     public void content(final T value) {
