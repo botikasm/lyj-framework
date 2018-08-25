4602e7df830ef178f8ba3e36ff0cb49e47d664f1
gian angelo geminiani <angelo.geminiani@gmail.com>
2018-08-25 18:31:00 +0200

    no message

--- projects/os_enterprise/ose_server/src/org/ly/ose/server/application/controllers/license/LicenseController.java
+++ projects/os_enterprise/ose_server/src/org/ly/ose/server/application/controllers/license/LicenseController.java
@@ -1,7 +1,100 @@
 package org.ly.ose.server.application.controllers.license;
 
+import org.lyj.commons.util.FileUtils;
+import org.lyj.commons.util.PathUtils;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Multi-Project License Controller.
+ */
 public class LicenseController {
 
+    // ------------------------------------------------------------------------
+    //                      c o n s t
+    // ------------------------------------------------------------------------
+
+    private static final String ROOT = "./licenses";
+
+    // ------------------------------------------------------------------------
+    //                      f i e l d s
+    // ------------------------------------------------------------------------
+
+    private final Map<String, LicenseRegistry> _licenses; // one registry for each license project
+    private final String _root;
+
+    // ------------------------------------------------------------------------
+    //                      c o n s t r u c t o r
+    // ------------------------------------------------------------------------
+
+    private LicenseController() {
+        _licenses = Collections.synchronizedMap(new HashMap<>());
+        _root = PathUtils.getAbsolutePath(ROOT);
+
+        this.init();
+    }
+
+    // ------------------------------------------------------------------------
+    //                      p u b l i c
+    // ------------------------------------------------------------------------
+
+    public void refresh() {
+        synchronized (_licenses) {
+            this.init();
+
+            _licenses.forEach((key, value) -> {
+                value.refresh();
+            });
+        }
+    }
+
+    public LicenseRegistry registry(final String project_key) {
+        return this.registry(project_key, true);
+    }
+
+    public LicenseRegistry registry(final String project_key,
+                                    final boolean auto_add) {
+        synchronized (_licenses) {
+            LicenseRegistry registry = _licenses.get(project_key);
+            if (null == registry && auto_add) {
+                final String project_path = PathUtils.combine(_root, project_key);
+                FileUtils.tryMkdirs(project_path);
+                registry = new LicenseRegistry(project_path);
+                _licenses.put(project_key, registry);
+            }
+            return registry;
+        }
+    }
+
+    // ------------------------------------------------------------------------
+    //                      p r i v a t e
+    // ------------------------------------------------------------------------
+
+    private void init() {
+        _licenses.clear();
+        final File[] projects = FileUtils.listDirs(_root);
+        for (final File project : projects) {
+            final String project_key = project.getName();
+            final String project_path = PathUtils.combine(_root, project_key);
+            _licenses.put(project_key, new LicenseRegistry(project_path));
+        }
+    }
+
+    // ------------------------------------------------------------------------
+    //                      S I N G L E T O N
+    // ------------------------------------------------------------------------
+
+    private static LicenseController __instance;
+
+    public static LicenseController instance() {
+        if (null == __instance) {
+            __instance = new LicenseController();
+        }
+        return __instance;
+    }
 
 
 }
---
+++ projects/os_enterprise/ose_server/src/org/ly/ose/server/application/controllers/license/LicenseItem.java
@@ -0,0 +1,176 @@
+package org.ly.ose.server.application.controllers.license;
+
+import org.lyj.commons.util.DateUtils;
+import org.lyj.commons.util.DateWrapper;
+import org.lyj.commons.util.FormatUtils;
+import org.lyj.commons.util.MathUtils;
+import org.lyj.commons.util.json.JsonItem;
+
+import java.util.Date;
+
+public class LicenseItem
+        extends JsonItem {
+
+    // ------------------------------------------------------------------------
+    //                      c o n s t
+    // ------------------------------------------------------------------------
+
+    private static final String FLD_UID = "uid";
+    private static final String FLD_TIMESTAMP = "timestamp";
+    private static final String FLD_DURATION_DAYS = "duration_days";
+    private static final String FLD_ENABLED = "enabled";
+    private static final String FLD_EMAIL = "email";
+    private static final String FLD_NAME = "name";
+    private static final String FLD_LANG = "lang";
+    private static final String FLD_FMT_EXPIRATION = "fmt_expiration";
+
+    // ------------------------------------------------------------------------
+    //                      c o n s t r u c t o r
+    // ------------------------------------------------------------------------
+
+    public LicenseItem() {
+        super();
+        this.init();
+    }
+
+    public LicenseItem(final Object item) {
+        super(item);
+        this.init();
+    }
+
+    // ------------------------------------------------------------------------
+    //                      p r o p e r t i e s
+    // ------------------------------------------------------------------------
+
+    public long timestamp() {
+        return super.getLong(FLD_TIMESTAMP);
+    }
+
+    public String uid() {
+        return super.getString(FLD_UID);
+    }
+
+    public LicenseItem uid(final String value) {
+        super.put(FLD_UID, value);
+        return this;
+    }
+
+    public String lang() {
+        return super.getString(FLD_LANG);
+    }
+
+    public LicenseItem lang(final String value) {
+        super.put(FLD_LANG, value);
+        return this;
+    }
+
+    public String email() {
+        return super.getString(FLD_EMAIL);
+    }
+
+    public LicenseItem email(final String value) {
+        super.put(FLD_EMAIL, value);
+        return this;
+    }
+
+    public String name() {
+        return super.getString(FLD_NAME);
+    }
+
+    public LicenseItem name(final String value) {
+        super.put(FLD_NAME, value);
+        return this;
+    }
+
+    public int durationDays() {
+        return super.getInt(FLD_DURATION_DAYS);
+    }
+
+    public LicenseItem durationDays(final int value) {
+        super.put(FLD_DURATION_DAYS, value);
+        return this;
+    }
+
+    public boolean enabled() {
+        return super.getBoolean(FLD_ENABLED);
+    }
+
+    public LicenseItem enabled(final boolean value) {
+        super.put(FLD_ENABLED, value);
+        return this;
+    }
+
+    // ------------------------------------------------------------------------
+    //                      p u b l i c
+    // ------------------------------------------------------------------------
+
+    public void postpone(final int days) {
+        final int duration = this.durationDays();
+        this.durationDays(duration + days);
+
+        this.recalculate();
+    }
+
+    public void expirationDate(final String date) {
+        try {
+            final DateWrapper dt = new DateWrapper(date, DateWrapper.DATEFORMAT_DEFAULT);
+            expirationDate(dt.getTime());
+        } catch (Throwable ignored) {
+        }
+    }
+
+    public void expirationDate(final Date date) {
+        this.expirationDate(date.getTime());
+    }
+
+    public void expirationDate(final long date) {
+        final long timestamp = this.timestamp(); // start of license
+        final long duration_ms = date - timestamp;
+        final int duration_day = MathUtils.floor(duration_ms / DateUtils.ONE_DAY_MS);
+
+        this.durationDays(duration_day);
+    }
+
+    public boolean expired() {
+        try {
+            final long now = System.currentTimeMillis();
+            final long expiration_time = this.expirationTime();
+
+            return expiration_time < now;
+        } catch (Throwable ignored) {
+            // ignored
+        }
+        return true;
+    }
+
+    // ------------------------------------------------------------------------
+    //                      p r i v a t e
+    // ------------------------------------------------------------------------
+
+    private void init() {
+        if (!super.has(FLD_TIMESTAMP)) {
+            super.put(FLD_TIMESTAMP, System.currentTimeMillis());
+        }
+        if (!super.has(FLD_ENABLED)) {
+            super.put(FLD_ENABLED, false);
+        }
+
+        this.recalculate();
+    }
+
+    private long durationMs() {
+        return DateUtils.ONE_DAY_MS * this.durationDays();
+    }
+
+    private long expirationTime() {
+        final long timestamp = this.timestamp();
+        final long duration_ms = this.durationMs();
+
+        return timestamp + duration_ms;
+    }
+
+    private void recalculate(){
+        final String fmt_date = FormatUtils.formatDate(new Date(this.expirationTime()));
+        super.put(FLD_FMT_EXPIRATION, fmt_date);
+    }
+}
